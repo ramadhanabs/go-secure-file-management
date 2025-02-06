fac664a61794000756720637a1f722300b6be6db
Ramadhana Bagus Solichuddin <ramadhana.solichuddin@erajaya.com>
2025-02-06 19:04:22 +0700

    chore: init repo

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+import { Button } from "./components/ui/button"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <></>
+}
+
+export default App
